19243f161aa6fd44ea2a1786244831497de2f02c
Paulsalinas1 <paul.salinasc@duocuc.cl>
2025-05-19 15:34:25 -0400

    atra estupides

--- src/app/layout/pages/dashboard/dashboard.component.ts
+++ src/app/layout/pages/dashboard/dashboard.component.ts
@@ -8,12 +8,16 @@ import { ChartType } from 'angular-google-charts';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  today = new Date();
   chartTitle = 'Ventas Mensuales';
   chartType = ChartType.PieChart;
   chartData = [
-    ['Enero', 1000],
-    ['Febrero', 1170],
-    ['Marzo', 660]
-  ];
-  chartOptions = { legend: { position: 'bottom' } };
+  ['Enero', 1000],
+  ['Febrero', 1170],
+  ['Marzo', 660]
+];
+  chartOptions = { legend:{ position: 'bottom' },
+  chartArea: { width: '100%', height: '80%' }
+
+};
 }
