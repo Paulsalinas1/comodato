f15da37ff8d24c3ef1cd46b7749cf7d24ec3feda
Paulsalinas1 <paul.salinasc@duocuc.cl>
2025-05-26 09:24:57 -0400

    eliminar cosas antiguas

--- src/app/layout/pages/articulos/articulos.module.ts
+++ src/app/layout/pages/articulos/articulos.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { ArticulosRoutingModule } from './articulos-routing.module';
 import { ArticulosComponent } from './articulos.component';
-import { ListaComponent } from './lista/lista.component';
 import { LayoutModule } from '../../layout.module';
 import { GoogleChartsModule } from 'angular-google-charts';
 import { NgxPaginationModule } from 'ngx-pagination';
@@ -14,7 +13,6 @@ import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     ArticulosComponent,
-    ListaComponent
   ],
   imports: [
     CommonModule,
