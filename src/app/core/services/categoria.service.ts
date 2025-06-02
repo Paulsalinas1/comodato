191d5cc8bacfb5f9e3ffb44202571daff23b3e05
Paulsalinas1 <paul.salinasc@duocuc.cl>
2025-06-02 10:00:56 -0400

    probando el inject

--- src/app/core/services/categoria.service.ts
+++ src/app/core/services/categoria.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Categoria } from '../models/categoria';
@@ -8,9 +8,9 @@ import { Categoria } from '../models/categoria';
   providedIn: 'root'
 })
 export class CategoriaService {
-  private apiUrl = 'http://localhost:3000/api/categorias';
-
-  constructor(private http: HttpClient) {}
+  apiUrl = 'http://localhost:3000/api/categorias';
+  http = inject(HttpClient)
+  constructor() {}
 
   getCategorias(): Observable<Categoria[]> {
     return this.http.get<Categoria[]>(this.apiUrl);
@@ -21,10 +21,12 @@ export class CategoriaService {
   }
 
   updateCategoria(id: string, categoria: Categoria): Observable<Categoria> {
-  return this.http.put<Categoria>(`${this.apiUrl}/${id}`, categoria);
-}
+    return this.http.put<Categoria>(`${this.apiUrl}/${id}`, categoria);
+  }
 
   deleteCategoria(id: string): Observable<void> {
-  return this.http.delete<void>(`${this.apiUrl}/${id}`);
-}
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+
+  
 }
