d4ed9f2c22217a773138554842f7d310afae5334
Paulsalinas1 <paul.salinasc@duocuc.cl>
2025-07-14 09:46:09 -0400

    devolucion

---
+++ src/app/core/models/Devolucion.ts
@@ -0,0 +1,24 @@
+export interface DevolucionComodato {
+     idDevolucion_comodato?: string;
+     nombre_completo_d: string;
+     rut_p_d: string;
+     cargo_d: string;
+     nombre_articulo_1: string;
+     nombre_articulo_2?: string;
+     nombre_articulo_3?: string;
+     nombre_articulo_4?: string;
+     nombre_articulo_5?: string;
+     nombre_articulo_6?: string;
+     motivo_d: string;
+     obsevacion_d: string;
+     nombre_r_d: string;
+     fecha_d: Date;
+     estado_a_1: string;
+     estado_a_2?: string;
+     estado_a_3?: string;
+     estado_a_4?: string;
+     estado_a_5?: string;
+     estado_a_6?: string;
+     Comodato_idComodato: string;
+  
+}
---
+++ src/app/core/services/devolucion_comodato.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DevolucionComodato } from '../models/Devolucion'; 
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class DevolucionComodatoService {
+  private readonly apiUrl = 'http://10.9.1.28:3000/api/devoluciones';
+
+  constructor(private readonly http: HttpClient) {}
+
+  getDevoluciones(): Observable<DevolucionComodato[]> {
+    return this.http.get<DevolucionComodato[]>(this.apiUrl);
+  }
+
+  getDevolucionById(id: string): Observable<DevolucionComodato> {
+    return this.http.get<DevolucionComodato>(`${this.apiUrl}/${id}`);
+  }
+
+  createDevolucion(devolucion: DevolucionComodato): Observable<{ message: string; idDevolucion_comodato: string }> {
+    return this.http.post<{ message: string; idDevolucion_comodato: string }>(this.apiUrl, devolucion);
+  }
+
+  updateDevolucion(id: string, devolucion: DevolucionComodato): Observable<{ message: string }> {
+    return this.http.put<{ message: string }>(`${this.apiUrl}/${id}`, devolucion);
+  }
+
+  deleteDevolucion(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
